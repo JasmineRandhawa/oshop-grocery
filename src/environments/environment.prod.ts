1fc1e5ed0f4ec6d06053b2a9e7977ce184642710
Jasmine Randhawa <55925330+JasmineRandhawa@users.noreply.github.com>
2021-07-06 18:23:24 -0600

    initial setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:
+  {
+    apiKey: "AIzaSyD7XNsiuyYp1F4IivTgXtsr3txInDBYb6E",
+    authDomain: "oshop-grocery.firebaseapp.com",
+    databaseURL: "https://oshop-grocery-default-rtdb.firebaseio.com",
+    projectId: "oshop-grocery",
+    storageBucket: "oshop-grocery.appspot.com",
+    messagingSenderId: "732452696677",
+    appId: "1:732452696677:web:d5788bcfce935e870b8c8c"
+  }
 };
